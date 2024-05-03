030a4fcdb134cc16adf2819d90c346252ab9ebb3
Animesh452 <animeshk452@gmail.com>
2024-05-03 12:30:08 +0530

    Name updated

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def user_input(user_question):
 # Main function
 def main():
     st.set_page_config("Chat PDF")  # Set the page config for the Streamlit app
-    st.header("Chat with PDF using Gemini💁")  # Display the header for the Streamlit app
+    st.header("AskPDF - LLM-driven PDF Question Answering System💁")  # Display the header for the Streamlit app
     user_question = st.text_input("Ask a Question from the PDF Files")  # Get user input
     if user_question:
         user_input(user_question)  # Call the user_input function with the user question
